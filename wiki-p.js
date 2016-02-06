3882115dda6408625a3b12ae7986d66eb4628462
Brian Stovall <hoppenmaus@gmail.com>
2016-02-05 21:52:07 -0500

    got jsonp working and started programatically generating entries.

--- wiki-p.js
+++ wiki-p.js
@@ -1,21 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var grab = document.getElementById.bind(document);
 	var searchTerm = grab('search-term');
+	var resultContainer = grab('result-container');
 
 	var apiCallPrefix='https://en.wikipedia.org/w/api.php?format=json' +
-	'&action=query&list=search&callback=test&srsearch=';
+	'&action=query&list=search&callback=populate&srsearch=';
 
-	test = function(data) {
-		alert(data);
+	//storage for the last search value - so as not to repeat searches
+	var lastValue;
+
+	//build a element for every search result
+	populate = function(data) {
+		data=data.query.search;
+		for (var i = 0; i < data.length; i++) {
+			console.log(data[i]);
+			var elem = document.createElement('div');
+			//elem.src = data[i].title;
+			elem.classList.add('infoDiv');
+			console.log(data[i].title);
+			elem.textContent=data[i].title;
+			resultContainer.appendChild(elem);
+		}
+
 	}
 
 	//send the entered value to make a request when user presses enter
 	searchTerm.onkeyup = function (event) {
-		if (event.key === 'Enter') {
-			var tempscript = document.createElement('script');
-			tempscript.id='tempscript';
-			tempscript.src = apiCallPrefix + this.value;
-			document.body.appendChild(tempscript);
+		if (event.key === 'Enter' && this.value !== lastValue) {
+			lastValue = this.value;
+			var jsonp = document.createElement('script');
+			jsonp.id='tempscript';
+			jsonp.src = apiCallPrefix + this.value;
+			document.body.appendChild(jsonp);
 		}
 	};
 
