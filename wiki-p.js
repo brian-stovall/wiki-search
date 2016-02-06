04a70921ec918d8867d9f28e5cb711703ab8aef2
Brian Stovall <hoppenmaus@gmail.com>
2016-02-06 18:23:24 -0500

    resized and respaced elements. commented and refactored javascript.

--- wiki-p.js
+++ wiki-p.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+	//references for important DOM objects
 	var grab = document.getElementById.bind(document);
 	var searchTerm = grab('search-term');
 	var resultContainer = grab('result-container');
@@ -10,22 +11,33 @@ document.addEventListener('DOMContentLoaded', function () {
 	var lastValue;
 
 	//build a element for every search result
-	populate = function(data) {
+	populate = function(JSON_served) {
+
 		var linkPrefix = 'https://en.wikipedia.org/?title=';
-		data=data.query.search;
+		//query.search is the part of the JSON that contains 
+		//the data needed
+		var data=JSON_served.query.search;
 
 		for (var i = 0; i < data.length; i++) {
+			//a wrapper to make the whole well a link
 			var link = document.createElement('a');
 			link.href = linkPrefix + data[i].title;
 			link.classList.add('link');
 
+			//create the outer element to go in resultContainer
 			var elem = document.createElement('div');
 			elem.classList.add('infoDiv');
 			elem.classList.add('well');
+
+			//the article title
 			var title = document.createElement('h2');
 			title.textContent=data[i].title;
+
+			//the summary, which is served as html
 			var snippet = document.createElement('p');
 			snippet.innerHTML = data[i].snippet;
+
+			//put
 			link.appendChild(elem);
 			elem.appendChild(title);
 			elem.appendChild(snippet);
@@ -35,9 +47,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	//send the entered value to make a request when user presses enter
+	//and has requested something different from last time
 	searchTerm.onkeyup = function (event) {
 		if (event.key === 'Enter' && this.value !== lastValue) {
+			//update lastValue on success
 			lastValue = this.value;
+
+			//get our jsonp
 			var jsonp = document.createElement('script');
 			jsonp.id='tempscript';
 			jsonp.src = apiCallPrefix + this.value;
